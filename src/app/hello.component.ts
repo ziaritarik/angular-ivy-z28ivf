265f741780dbf7b7bbf6b6d1b35b637495dee5cd
ziaritarik <58822986+ziaritarik@users.noreply.github.com>
2022-10-10 23:20:31 +0200

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,20 +2,45 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>{{countere}}Hello {{message}} {{counter}}!</h1>`,
+  template: `<h1>{{getMessage()}} !!Hello {{message}} {{counter}}!</h1>
+  
+  <p> {{LongText}}</p>
+  <p [textContent]=LongText> </p>
+
+  <p textContent=LongText> </p>
+  <p textContent={{LongText}}> </p>
+
+  
+  <input [value]="message"/>
+  <input value={{message}}/>
+  <br><br>
+  <button  disabled='disabled'>Submit</button>
+  <br>
+  <button  [disabled]='disabled'>Submit</button>
+  <br><br>
+
+  <input [value]="getMessage()"/>
+
+  `,
   styles: [`h1 { font-family: Lato; }`],
 })
 export class HelloComponent {
   message = 'world';
   counter = 0;
-  countere = 0;
+  LongText = 'Lorem Ipsum';
+  disabled = false;
   constructor() {
+    /*
     setInterval(() => {
       this.counter++;
     }, 1000);
 
     setInterval(() => {
       this.countere--;
-    }, 1000);
+    }, 1000);*/
+  }
+
+  getMessage() {
+    return 'world';
   }
 }
