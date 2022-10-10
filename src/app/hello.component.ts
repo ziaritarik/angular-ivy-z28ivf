c9eec2f4ecae192fff2bdc16b4f68d52d5fb8691
ziaritarik <58822986+ziaritarik@users.noreply.github.com>
2022-10-10 20:13:01 +0200

    2 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,9 +2,20 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `<h1>{{countere}}Hello {{message}} {{counter}}!</h1>`,
+  styles: [`h1 { font-family: Lato; }`],
 })
-export class HelloComponent  {
-  @Input() name: string;
+export class HelloComponent {
+  message = 'world';
+  counter = 0;
+  countere = 0;
+  constructor() {
+    setInterval(() => {
+      this.counter++;
+    }, 1000);
+
+    setInterval(() => {
+      this.countere--;
+    }, 1000);
+  }
 }
